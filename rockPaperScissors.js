281cf0d3fc0baf1ac70888cdf01bb484b677138c
Oli Cannoli <cannolikraken@gmail.com>
2022-08-10 21:41:19 -0400

    Add getComputerChoice function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,19 @@
-console.log("Hello world.");
+let playAsk = prompt("Hello world. Care for a game?");
+
+function getComputerChoice(){
+    choiceNumber = Math.floor(Math.random() * 3);
+    if (choiceNumber == 0){
+        computerChoice = "rock"
+    }
+    if (choiceNumber == 1){
+        computerChoice = "paper"
+    }
+    if (choiceNumber == 2){
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
