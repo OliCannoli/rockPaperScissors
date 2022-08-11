45f4f64f6614b8b287e159130971a84a86abe2d3
Oli Cannoli <cannolikraken@gmail.com>
2022-08-10 22:03:27 -0400

    Add playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,8 +9,49 @@ function getComputerChoice(){
             break;
         case 2:
             computerChoice = "scissors"
-            break;
     }
     return computerChoice;
 }
 
+function playRound(playerSelection){
+    let computerSelection = getComputerChoice()
+    console.log("Player chose " + playerSelection + ", computer chose " + computerSelection);
+    switch (playerSelection){
+        case "rock":
+            switch (computerSelection){
+                case "rock":
+                    return "It's a tie!";
+                    break;
+                case "paper":
+                    return "You lose! Paper beats rock.";
+                    break;
+                case "scissors":
+                    return "Rock beats scissors! You win!";
+            }
+        case "paper":
+            switch (computerSelection){
+                case "rock":
+                    return "Rock beats paper! You win!";
+                    break;
+                case "paper":
+                    return "It's a tie!";
+                    break;
+                case "scissors":
+                    return "You lose! Scissors beat paper.";
+            }
+        case "scissors":
+            switch (computerSelection){
+                case "rock":
+                    return "You lose! Rock beats scissors.";
+                    break;
+                case "paper":
+                    return "Scissors beat paper! You win!";
+                    break;
+                case "scissors":
+                    return "It's a tie!";
+            }
+        default:
+            return "Invalid input."
+    }
+}
+console.log(playRound(prompt("Hello world. Care for a game? Choose rock, paper, or scissors.").toLowerCase()))
